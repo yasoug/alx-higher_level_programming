e570731428729a01407511dad9b311c9cc8596d5
yasoug <ougouisyassine@gmail.com>
2023-06-14 13:42:02 -0700

    task 13 : commit 2 (case weights = 0)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list is not None:
-        return(sum(a*b for a, b in my_list)/sum(b for a, b in my_list))
+        f = sum(b for a, b in my_list)
+        return (sum(a*b for a, b in my_list)/f) if f > 0 else 0
     return (0)
