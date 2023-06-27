f2591d37f5f5c97d5df515675d4fb1d084ad684b
yasoug <ougouisyassine@gmail.com>
2023-06-27 10:46:56 -0700

    task 1 : commit 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class that defines a square"""
+
+
+class Square:
+    """class that defines a square with attribute"""
+
+    def __init__(self, size):
+        """
+        Initializes the square
+
+        Attributes:
+            size: the size of a side of a square
+        """
+        self.__size = size
