5e3fc2341430fb3196ac9313667ee2e40e01089a
yasoug <ougouisyassine@gmail.com>
2023-06-27 11:34:31 -0700

    task 4 : commit 1

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""class that defines a square"""
+
+
+class Square:
+    """class that defines a square with attribute"""
+
+    def __init__(self, size=0):
+        """
+        Initializes the square
+
+        Attributes:
+            size (int): the size of a side of a square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter : returns the size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter : set the size attribute"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Public instance : that returns the square area"""
+        return (self.__size)**2
