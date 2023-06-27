5e35369e00066a2c6192b07ecac0359de5db21ab
yasoug <ougouisyassine@gmail.com>
2023-06-27 11:19:39 -0700

    task 3 : commit 1

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""class that defines a square"""
+
+
+class Square:
+    """class that defines a square with attribute"""
+
+    def __init__(self, size=0):
+        """
+        Initializes the square
+
+        Attributes:
+            size (int): the size of a side of a square
+        Raises:
+            TypeError: size is not a int
+            ValueError: size is less than 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance : that returns the square area"""
+        return (self.__size)**2
