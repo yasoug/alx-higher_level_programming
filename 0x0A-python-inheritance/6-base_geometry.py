82f63d83fa63d1efa4d8cdd911d8d3da4807694e
yasoug <ougouisyassine@gmail.com>
2023-07-10 11:43:41 -0700

    task 6 : commit 1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
