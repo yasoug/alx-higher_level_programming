03af273c808ac258c617271cc611ee352eb16325
yasoug <ougouisyassine@gmail.com>
2023-07-10 11:32:48 -0700

    task 4 : commit 1

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class checks if obj is an instance of a class that inherited from"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False
+    """
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
