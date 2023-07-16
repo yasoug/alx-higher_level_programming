1088984a5624b93f77531151c4afbc32b9b3bded
yasoug <ougouisyassine@gmail.com>
2023-07-16 06:29:08 -0700

    task 6 : commit 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,8 @@ class Rectangle(Base):
         """prints the rectangle instance with the character #"""
         for i in range(self.__height):
             print(self.__width * "#")
+
+    def __str__(self):
+        """prints [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
