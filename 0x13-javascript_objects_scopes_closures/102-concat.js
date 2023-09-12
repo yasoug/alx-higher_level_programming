8b6c83df0044cfe221e37600b56b2891a02a60bf
yasoug <ougouisyassine@gmail.com>
2023-09-12 13:51:15 -0700

    task 13 : commit 1

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+const file = require('fs');
+const textA = file.readFileSync(fileA, 'utf8');
+const textB = file.readFileSync(fileB, 'utf8');
+file.writeFileSync(fileC, textA + textB);
