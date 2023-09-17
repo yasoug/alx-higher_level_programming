1704d33209aa659bb6f76242b8eddc41a3476227
yasoug <ougouisyassine@gmail.com>
2023-09-17 11:18:30 -0700

    task 15 : commit 2 (correction)

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,6 +10,7 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     new_state = State(name="California")
