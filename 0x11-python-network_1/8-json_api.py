7a2840300dfc1240341af8bd9225f6c0642a4c4b
yasoug <ougouisyassine@gmail.com>
2023-09-29 08:38:50 -0700

    task 8 : commit 1

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        q = ""
+    else:
+        q = argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    value = {'q': q}
+    response = requests.post(url, data=value)
+    try:
+        json = response.json()
+        if json:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
