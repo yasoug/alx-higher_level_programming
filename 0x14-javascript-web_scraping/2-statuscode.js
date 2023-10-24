cb49aa1fd684c1b1641ea1d3cbdb072db6dcb4a6
yasoug <ougouisyassine@gmail.com>
2023-10-24 12:06:37 -0700

    task 2 : commit 2 (remove space)

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,6 +7,6 @@ request(url, (err, response) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
