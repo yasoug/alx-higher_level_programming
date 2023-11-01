8b1ed68803f0f5f0ce95f8b54c342914c8f2a553
yasoug <ougouisyassine@gmail.com>
2023-11-01 09:54:39 -0700

    task 12 : commit 1

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// This script updates the text color of header to red (#FF0000)
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('header').style.color = '#FF0000';
+  });
+})();
